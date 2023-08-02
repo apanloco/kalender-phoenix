cdde136032a53881273ee165b69b49f56be4fc61
Daniel Åkerud <daniel.akerud@gmail.com>
2023-08-02 09:26:55 +0200

    Validate year and month in controller

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 Start Phoenix endpoint wxith `mix phx.server`.
 
 Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
+
+## TODO
+
+[ ] remove unused components, javascript, etc
--- lib/kalender_web/controllers/kalender_controller.ex
+++ lib/kalender_web/controllers/kalender_controller.ex
@@ -2,22 +2,31 @@ defmodule KalenderWeb.KalenderController do
   use KalenderWeb, :controller
   alias HTTPoison
 
-  def index(conn, %{"year" => year, "month" => month}) do
-    case HTTPoison.get("https://calendar-api.akerud.se/#{year}/#{month}") do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        data = body |> Jason.decode!()
-        render(conn, "index.html", layout: false, year: year, month: month, data: data)
+  def index(conn, %{"year" => year_string, "month" => month_string}) do
+    with {year, ""} <- Integer.parse(year_string),
+         {month, ""} <- Integer.parse(month_string),
+         true <- year in 1903..2998,
+         true <- month in 1..12 do
+      case HTTPoison.get("https://calendar-api.akerud.se/#{year}/#{month}") do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          data = body |> Jason.decode!()
+          render(conn, "index.html", layout: false, year: year, month: month, data: data)
 
-      {:ok, %HTTPoison.Response{status_code: status_code}} ->
-        send_resp(conn, status_code, "Something went wrong")
+        {:ok, %HTTPoison.Response{status_code: status_code}} ->
+          send_resp(conn, 500, "Something went wrong")
 
-      {:error, %HTTPoison.Error{reason: _reason}} ->
-        send_resp(conn, 500, "Internal server error")
+        {:error, %HTTPoison.Error{reason: _reason}} ->
+          send_resp(conn, 500, "Internal server error")
+      end
+    else
+      _ -> send_resp(conn, 400, "Invalid year or month")
     end
   end
 
   def home(conn, _params) do
     date = DateTime.utc_now()
-    redirect(conn, to: ~p"/#{Map.fetch!(date, :year)}/#{Map.fetch!(date, :month)}")
+    year = Map.fetch!(date, :year)
+    month = Map.fetch!(date, :month)
+    redirect(conn, to: ~p"/#{year}/#{month}")
   end
 end
